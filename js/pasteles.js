72c1da2a195cb3b3464b6155e9cd38d50fd1b1f3
RicardoHernandezVarela <rhvarela011192@gmail.com>
2019-12-31 16:12:20 -0600

    CSS de los componenetes

---
+++ js/pasteles.js
@@ -0,0 +1,19 @@
+let pasteles = {
+    Cocoa:{
+        tipo: 'Cocoa',
+        precio: '$450',
+    }, 
+
+    Cajeta:{
+        tipo: 'Cajeta',
+        precio: '$440'
+    },
+
+    Limon: {
+        tipo: 'Limón',
+        precio: '$430'
+    }
+
+};
+
+export default pasteles;
